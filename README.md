4c33f377ea233aa087eabe2b55e32db6e9c1c22c
Marvin Isaac <4396841+marvinisaac@users.noreply.github.com>
2023-01-02 09:33:04 +0000

    :alembic: Merge Phoenix Project in

--- README.md
+++ README.md
@@ -3,5 +3,12 @@
 An Ansible-based setup for Ubuntu Server
 
 ## How to Use
-1. Uncomment play/s in `playbook.yaml`
-2. Start setup with `bash setup.sh`
+1. SSH/open a console to the machine
+2. Clone repo and move to the directory
+3. Create config file by copying `config.dist` to `config`
+    - To get the interface value, run `ip a`
+    - Known default values are:
+        - Proxmox + Ubuntu: `ens18`
+        - VirtualBox + Ubuntu: `enp0s3`
+4. Start setup with `bash setup.sh`
+5. Reboot machine
--- setup.sh
+++ setup.sh
@@ -5,8 +5,16 @@ sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible --yes
 
-ansible-playbook ./playbook.yaml -K \
-    -e "target=localhost"
+# Get values from config
+export $(cat $(dirname -- "$0")/config | xargs)
 
+ansible-playbook ./playbook.yaml -K \
+    -e "target=localhost \
+        gateway=$DEV_IP_GATEWAY \
+        interface=$DEV_IP_INTERFACE \
+        ip=$DEV_IP_ADDRESSS \
+    "
+
+# Run docker commands without sudo
 sudo usermod -aG docker $USER
 newgrp docker
